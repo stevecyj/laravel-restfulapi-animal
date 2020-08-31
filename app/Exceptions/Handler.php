5b715714b9c687be877c8233c407678fb9ba73fc
Steve <ycsteven.tsao@gmail.com>
2020-09-01 03:38:37 +0800

    修正Handler Response error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,62 +6,64 @@ use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+// 要 Response 的話要加這段
+use Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+  /**
+   * A list of the exception types that are not reported.
+   *
+   * @var array
+   */
+  protected $dontReport = [
+    //
+  ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
+  /**
+   * A list of the inputs that are never flashed for validation exceptions.
+   *
+   * @var array
+   */
+  protected $dontFlash = [
+    'password',
+    'password_confirmation',
+  ];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
+  /**
+   * Report or log an exception.
+   *
+   * @param  \Exception  $exception
+   * @return void
+   */
+  public function report(Exception $exception)
+  {
+    parent::report($exception);
+  }
+
+  /**
+   * Render an exception into an HTTP response.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Exception  $exception
+   * @return \Illuminate\Http\Response
+   */
+  public function render($request, Exception $exception)
+  {
+    // 讓它顯示一下是哪一個異常狀況
+    // dd($exception);
+
+    if ($request->expectsJson()) {
+      if ($exception instanceof ModelNotFoundException) {
+        return response()->json(
+          [
+            'error' => '找不到資源'
+          ],
+          Response::HTTP_NOT_FOUND
+        );
+      }
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        // 讓它顯示一下是哪一個異常狀況
-        // dd($exception);
-
-        if ($request->expectsJson()) {
-          if ($exception instanceof ModelNotFoundException) {
-              return response()->json(
-                  [
-                      'error' => '找不到資源'
-                  ],
-                  Response::HTTP_NOT_FOUND
-                  );
-              }
-          }
-
-        return parent::render($request, $exception);
-    }
+    return parent::render($request, $exception);
+  }
 }
